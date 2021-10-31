c574705d0fdf81ebfc698d20eaa1aa3dc933a393
Mauricio <mauriciolopezasencio@gmail.com>
2021-10-31 15:00:25 -0600

    tabla de Usuario con procedimientos almacenados y login funcional.

---
+++ GUIPBD2/Login.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace GUIPBD2
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.usuarioTableAdapter = new GUIPBD2.PBDDataSetTableAdapters.UsuarioTableAdapter();
+            this.pbdDataSet = new GUIPBD2.PBDDataSet();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbdDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(332, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "INICIO DE SISTEMA DE ADMINISTRACION \r\nDE ALUMNOS Y EMPRESAS";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "USUARIO";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(13, 100);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(331, 20);
+            this.txtUsuario.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(16, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "PASSWORD";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(13, 155);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(331, 20);
+            this.txtPassword.TabIndex = 4;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // button1
+            // 
+            this.button1.Image = global::GUIPBD2.Properties.Resources.Login;
+            this.button1.Location = new System.Drawing.Point(70, 241);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 78);
+            this.button1.TabIndex = 5;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Image = global::GUIPBD2.Properties.Resources.Loguot;
+            this.button2.Location = new System.Drawing.Point(184, 241);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 78);
+            this.button2.TabIndex = 6;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // usuarioTableAdapter
+            // 
+            this.usuarioTableAdapter.ClearBeforeFill = true;
+            // 
+            // pbdDataSet
+            // 
+            this.pbdDataSet.DataSetName = "PBDDataSet";
+            this.pbdDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::GUIPBD2.Properties.Resources.INICIO_DE_SESION;
+            this.ClientSize = new System.Drawing.Size(356, 388);
+            this.ControlBox = false;
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.Transparent;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(372, 427);
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbdDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private PBDDataSetTableAdapters.UsuarioTableAdapter usuarioTableAdapter;
+        private PBDDataSet pbdDataSet;
+    }
+}
---
+++ GUIPBD2/Login.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUIPBD2
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //conectar a la base de datos y va a validar que exixte el usaurio
+            //validar si los campos estan llenos 
+            if (this.valida())
+            {
+                //mandar a llamar el procedimiento de usuario para validar si existe 
+                this.usuarioTableAdapter.Fill(this.pbdDataSet.Usuario, this.txtUsuario.Text.Trim(), this.txtPassword.Text.Trim());
+                //si existe el usuario 
+                if (this.pbdDataSet.Usuario.Rows.Count > 0)
+                {
+                    //existe el usuario ingresamos al menu principal
+                    Form1 f = new Form1();
+                    this.Hide();
+                    f.ShowDialog();
+                    this.Close();
+
+                }
+                else
+                {
+                    //tiene que mandar un mensaje diciendo que el usuario o la contraseña son incorrectos
+                    MessageBox.Show("El usuario o contraseña son incorrectos", "valida usuario", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+        }
+        private bool valida()
+        {
+            this.errorProvider1.Clear();
+            bool resultado = true;
+            //validar el campo de usuario 
+            if (txtUsuario.Text.Trim() == "")
+            {
+                resultado = false;
+                this.errorProvider1.SetError(this.txtUsuario, "este campo es requerido");
+            }
+            //validar el campo de password
+            if (txtUsuario.Text.Trim() == "")
+            {
+                resultado = false;
+                this.errorProvider1.SetError(this.txtPassword, "este campo es requerido");
+            }
+
+            return resultado;
+        }
+    }
+}
